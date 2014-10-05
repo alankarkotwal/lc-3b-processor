f4cd6f731a1671692813e0b27f85671b1d82b412
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-10-06 05:12:23 +0530

    completed regfile and added demux

---
+++ misc/demux.v
@@ -0,0 +1,17 @@
+module demux8(selectInput, out);  // 8-output demux
+	output reg [7:0] out;
+	input  [2:0] selectInput;
+	
+	always@(selectInput) begin
+		case(selectInput)
+			0: out = 16'b00000001;
+			1: out = 16'b00000010;
+			2: out = 16'b00000100;
+			3: out = 16'b00001000;
+			4: out = 16'b00010000;
+			5: out = 16'b00100000;
+			6: out = 16'b01000000;
+			7: out = 16'b10000000;
+		endcase
+	end
+endmodule
--- reg-file/register_file.v
+++ reg-file/register_file.v
@@ -1,20 +1,33 @@
 `include "register.v"
 `include "../misc/mux.v"
+`include "../misc/demux.v"
 
-module register_file(clk, out1, out2, readAdd1, readAdd2, write, writeAdd, in, reset); // clk, out, in, write, reset
+module register_file(clk, out1, out2, readAdd1, readAdd2, write, writeAdd, in, reset);
 	output [15:0] out1, out2;
 	input  [15:0] in;
 	input  [2:0]  readAdd1, readAdd2, writeAdd;
 	input         write, clk, reset;
 	
-	wire   [15:0] data0, data1, data2, data3, data4, data5, data6, data7;
+	wire   [15:0] data0,  data1,  data2,  data3,  data4,  data5,  data6,  data7;
+	wire   [7:0]  writeLinesInit, writeLines;
 	
-	register16 r0(clk, data0, in, reset);
-	register16 r1();
-	register16 r2();
-	register16 r3();
-	register16 r4();
-	register16 r5();
-	register16 r6();
-	register16 r7();
+	demux8 dem(writeAdd, writeLinesInit);
+	
+	and a0(writeLines[0], write, writeLinesInit[0]);
+	and a1(writeLines[1], write, writeLinesInit[1]);
+	and a2(writeLines[2], write, writeLinesInit[2]);
+	and a3(writeLines[3], write, writeLinesInit[3]);
+	and a4(writeLines[4], write, writeLinesInit[4]);
+	and a5(writeLines[5], write, writeLinesInit[5]);
+	and a6(writeLines[6], write, writeLinesInit[6]);
+	and a7(writeLines[7], write, writeLinesInit[7]);
+	
+	register16 r0(clk, data0, in, writeLines[0], reset);
+	register16 r1(clk, data1, in, writeLines[1], reset);
+	register16 r2(clk, data2, in, writeLines[2], reset);
+	register16 r3(clk, data3, in, writeLines[3], reset);
+	register16 r4(clk, data4, in, writeLines[4], reset);
+	register16 r5(clk, data5, in, writeLines[5], reset);
+	register16 r6(clk, data6, in, writeLines[6], reset);
+	register16 r7(clk, data7, in, writeLines[7], reset);
 endmodule
