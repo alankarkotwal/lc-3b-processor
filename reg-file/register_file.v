7672fcd6579d196080f818411645502b58e7811b
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-10-07 12:06:29 +0530

    corrected some mistakes in regfile

--- reg-file/register_file.v
+++ reg-file/register_file.v
@@ -12,15 +12,17 @@ module register_file(clk, out1, out2, readAdd1, readAdd2, write, writeAdd, in, r
 	wire   [7:0]  writeLinesInit, writeLines;
 	
 	demux8 dem(writeAdd, writeLinesInit);
+	mux16x4 mux1(data0, data1, data2, data3, data4, data5, data6, data7, readAdd1, out1);
+	mux16x4 mux2(data0, data1, data2, data3, data4, data5, data6, data7, readAdd2, out2);
 	
-	and a0(writeLines[0], write, writeLinesInit[0]);
-	and a1(writeLines[1], write, writeLinesInit[1]);
-	and a2(writeLines[2], write, writeLinesInit[2]);
-	and a3(writeLines[3], write, writeLinesInit[3]);
-	and a4(writeLines[4], write, writeLinesInit[4]);
-	and a5(writeLines[5], write, writeLinesInit[5]);
-	and a6(writeLines[6], write, writeLinesInit[6]);
-	and a7(writeLines[7], write, writeLinesInit[7]);
+	or a0(writeLines[0], write, ~writeLinesInit[0]);
+	or a1(writeLines[1], write, ~writeLinesInit[1]);
+	or a2(writeLines[2], write, ~writeLinesInit[2]);
+	or a3(writeLines[3], write, ~writeLinesInit[3]);
+	or a4(writeLines[4], write, ~writeLinesInit[4]);
+	or a5(writeLines[5], write, ~writeLinesInit[5]);
+	or a6(writeLines[6], write, ~writeLinesInit[6]);
+	or a7(writeLines[7], write, ~writeLinesInit[7]);
 	
 	register16 r0(clk, data0, in, writeLines[0], reset);
 	register16 r1(clk, data1, in, writeLines[1], reset);
