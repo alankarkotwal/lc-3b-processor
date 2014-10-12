21797006de3a70702905b67596d1c6489f7300d2
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-10-12 21:56:48 +0530

    modified rm/wmb control name to rmwmb

--- design/controller-gen.py
+++ design/controller-gen.py
@@ -27,18 +27,34 @@ for i in controlLines:
 outfile.write("module controller(clk, IR, N, Z, P")
 
 # Write control signals, outputs of FSM
-for i in range(1, len(controls[0])):
-
-	# Get header of signal iff header != NULL
-	if len(controls[0][i]) != 0:
-		outfile.write(", "+controls[0][i])
+controlSignals = []
+controlSignalLengths = []
+i=1
+while i<len(controls[0]):
+	# First write the control signal
+	outfile.write(", "+controls[0][i])
+	controlSignals.append(controls[0][i])
+	i=i+1
+	
+	# Increment i till you get a non-zero length
+	length = 0
+	while len(controls[0][i])==0:
+		length=length+1
+		i=i+1
+		if(i==len(controls[0])):
+			break
+	controlSignalLengths.append(length)
 
 outfile.write(");		// Implements the designed controller for LC-3b.\n\n")
 
-# Specify inputs. We have only IR and 
-outfile.write ("input [15:0] IR;\n\
-		input N, Z, P\n");
+# Specify inputs. We have only IR and CCR Flags
+outfile.write ("\tinput  [15:0] IR;\n\tinput  clk, N, Z, P;\n");
 
+for i in range(len(controlSignals)):
+	if controlSignalLengths[i]!=0:
+		outfile.write("\toutput ["+str(controlSignalLengths[i])+":0] "+controlSignals[i]+";\n")
+	else:
+		outfile.write("\toutput "+controlSignals[i]+";\n")
 
-
+outfile.write("\nendmodule");
 outfile.close()
--- design/controller.v
+++ design/controller.v
@@ -1,1 +1,28 @@
-module controller(clk, IR, N, Z, P, StateID, Mux1, Mux2, Mux3, Mux4, Mux5, Mux6, Mux7, Mux8, Mux9, Mux10, Mux11, Mux12, wrf, wpc, wir, lccr, aluop, alushop, rm/wmb, lalu, ltempdr);		// Implements the designed controller for LC-3b.
+module controller(clk, IR, N, Z, P, StateID, Mux1, Mux2, Mux3, Mux4, Mux5, Mux6, Mux7, Mux8, Mux9, Mux10, Mux11, Mux12, wrf, wpc, wir, lccr, aluop, alushop, rmwmb, lalu, ltempdr);		// Implements the designed controller for LC-3b.
+
+	input  [15:0] IR;
+	input  clk, N, Z, P;
+	output StateID;
+	output Mux1;
+	output [1:0] Mux2;
+	output [2:0] Mux3;
+	output [1:0] Mux4;
+	output [1:0] Mux5;
+	output [1:0] Mux6;
+	output [1:0] Mux7;
+	output Mux8;
+	output Mux9;
+	output Mux10;
+	output Mux11;
+	output Mux12;
+	output wrf;
+	output wpc;
+	output [1:0] wir;
+	output lccr;
+	output [1:0] aluop;
+	output [1:0] alushop;
+	output rmwmb;
+	output lalu;
+	output [3:0] ltempdr;
+
+endmodule
