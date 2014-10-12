2e8b19c5fcb7bd72ac1e85bcf354fbccf075f22b
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-10-12 22:56:46 +0530

    almost finished controller-gen

--- design/controller-gen.py
+++ design/controller-gen.py
@@ -3,17 +3,34 @@
 # Code to generate the controller for LC-3b
 # Author: Alankar Kotwal
 
+# Must ensure the following:
+# # The first line in the CSV is control signal names. In case of multiple lines per control signal, first column must be control signal name, rest nulls.
+# # State number 0 is always reset, state number 1 is always fetch1.
+# # All constant-logic signals are in misc.direc
+
+import math
+
+# Options
+_NEGEDGE =  0
+_XVAL = '0'
+
 # Open all necessary files
 statefile = open('states.csv', 'r')
 controlfile = open('controls.csv', 'r')
+opfile = open('opcodes.conf', 'r')
+miscfile = open('misc.direc', 'r')
 outfile = open('controller.v', 'w')
 
 # Get arrays
 stateLines = statefile.readlines()
 controlLines = controlfile.readlines()
+opLines = opfile.readlines()
+miscLines = miscfile.readlines()
 
 controlfile.close()
 statefile.close()
+opfile.close()
+miscfile.close()
 
 states = []
 for i in stateLines:
@@ -22,6 +39,10 @@ for i in stateLines:
 controls = []	
 for i in controlLines:
 	controls.append(i.rstrip().split(','))
+	
+opcodes = []	
+for i in opLines:
+	opcodes.append(i.rstrip().split(','))
 
 # First write header info
 outfile.write("module controller(clk, IR, N, Z, P")
@@ -43,6 +64,9 @@ while i<len(controls[0]):
 		i=i+1
 		if(i==len(controls[0])):
 			break
+			
+	if i==2:
+		length = int(math.ceil(math.log(len(controls)+1)))
 	controlSignalLengths.append(length)
 
 outfile.write(");		// Implements the designed controller for LC-3b.\n\n")
@@ -50,11 +74,50 @@ outfile.write(");		// Implements the designed controller for LC-3b.\n\n")
 # Specify inputs. We have only IR and CCR Flags
 outfile.write ("\tinput  [15:0] IR;\n\tinput  clk, N, Z, P;\n");
 
+# Specify outputs from controls file
 for i in range(len(controlSignals)):
 	if controlSignalLengths[i]!=0:
 		outfile.write("\toutput ["+str(controlSignalLengths[i])+":0] "+controlSignals[i]+";\n")
 	else:
 		outfile.write("\toutput "+controlSignals[i]+";\n")
 
-outfile.write("\nendmodule");
+# Assign constant signals
+outfile.write("\n")
+for i in miscLines:
+	outfile.write("\tassign "+i.rstrip()+";\n")
+
+# Start state machine
+if _NEGEDGE == 1:
+	outfile.write("\n\talways@(negedge clk) begin\n")
+else:
+	outfile.write("\n\talways@(posedge clk) begin\n")
+
+# Start switch-case
+outfile.write("\t\tcase("+controls[0][1]+") begin\n")
+
+for state in range(1, len(controls)):
+	outfile.write("\t\t\t"+str(state)+": begin\n")
+	signalNo = 2
+	for controlSignal in range(1, len(controlSignals)):
+		outfile.write("\t\t\t\t"+controlSignals[controlSignal]+" = "+str(controlSignalLengths[controlSignal]+1)+"'b")
+		signalNoHere = controlSignalLengths[controlSignal]
+		while signalNoHere>=0:
+			if(controls[state][signalNo]!='x'):
+				outfile.write(controls[state][signalNo])
+			else:
+				outfile.write(_XVAL)
+			signalNo = signalNo+1
+			signalNoHere = signalNoHere-1
+		outfile.write("\n")
+	
+	# Calculate next state here.
+	nextState = 0
+	outfile.write("\t\t\t\tstate = "+str(nextState)+"\n")
+	outfile.write("\t\t\tend\n")
+
+outfile.write("\t\tendcase\n")
+
+outfile.write("\tend\n")
+
+outfile.write("\nendmodule")
 outfile.close()
--- design/controller.v
+++ design/controller.v
@@ -2,7 +2,7 @@ module controller(clk, IR, N, Z, P, StateID, Mux1, Mux2, Mux3, Mux4, Mux5, Mux6,
 
 	input  [15:0] IR;
 	input  clk, N, Z, P;
-	output StateID;
+	output [4:0] StateID;
 	output Mux1;
 	output [1:0] Mux2;
 	output [2:0] Mux3;
@@ -24,5 +24,734 @@ module controller(clk, IR, N, Z, P, StateID, Mux1, Mux2, Mux3, Mux4, Mux5, Mux6,
 	output rmwmb;
 	output lalu;
 	output [3:0] ltempdr;
+
+	assign Mux8 =  IR[5];
+	assign Mux9 =  (IR[11] && N) || (IR[10] && Z) || (IR[9] && P);
+	assign Mux10 = IR[11];
+
+	always@(posedge clk) begin
+		case(StateID) begin
+			1: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b01
+				Mux5 = 2'b01
+				Mux6 = 2'b10
+				Mux7 = 2'b10
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b0
+				wir = 2'b10
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			2: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b01
+				Mux5 = 2'b01
+				Mux6 = 2'b10
+				Mux7 = 2'b10
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b0
+				wir = 2'b01
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			3: begin
+				Mux1 = 1'b1
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			4: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b0
+				aluop = 2'bIR[15]IR[14]
+				alushop = 2'bIR[5]IR[4]
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			5: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b11
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b0
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			6: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b011
+				Mux4 = 2'b01
+				Mux5 = 2'b00
+				Mux6 = 2'b10
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b0
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			7: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b01
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b0
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			8: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b10
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b1
+				Mux12 = 1'b0
+				wrf = 1'b0
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			9: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b001
+				Mux4 = 2'b01
+				Mux5 = 2'b00
+				Mux6 = 2'b01
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b0
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			10: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			11: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b01
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			12: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b10
+				Mux5 = 2'b10
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b0
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b0
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			13: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b010
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			14: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b01
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			15: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1110
+				state = 0
+			end
+			16: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b11
+				Mux5 = 2'b01
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			17: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b01
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			18: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1101
+				state = 0
+			end
+			19: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b01
+				Mux3 = 3'b000
+				Mux4 = 2'b10
+				Mux5 = 2'b10
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b0
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b0
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			20: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b011
+				Mux4 = 2'b01
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b0
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			21: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b11
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b0
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			22: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			23: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b101
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			24: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1011
+				state = 0
+			end
+			25: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b01
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b0
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			26: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b010
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			27: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b0011
+				state = 0
+			end
+			28: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b01
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b0
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+			29: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b11
+				Mux5 = 2'b01
+				Mux6 = 2'b00
+				Mux7 = 2'b00
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b0
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b0
+				ltempdr = 4'b1111
+				state = 0
+			end
+			30: begin
+				Mux1 = 1'b0
+				Mux2 = 2'b00
+				Mux3 = 3'b000
+				Mux4 = 2'b00
+				Mux5 = 2'b00
+				Mux6 = 2'b00
+				Mux7 = 2'b01
+				Mux8 = 1'bIR[5]
+				Mux9 = 1'b(IR[11] && N) || (IR[10] && Z) || (IR[9] && P)
+				Mux10 = 1'bIR[11]
+				Mux11 = 1'b0
+				Mux12 = 1'b1
+				wrf = 1'b1
+				wpc = 1'b1
+				wir = 2'b11
+				lccr = 1'b1
+				aluop = 2'b00
+				alushop = 2'b00
+				rmwmb = 1'b1
+				lalu = 1'b1
+				ltempdr = 4'b1111
+				state = 0
+			end
+		endcase
+	end
 
 endmodule
